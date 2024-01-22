e8ecb7a516f2cdb5718f9e33fc8b4a2c46ad0a65
Tracy Yang <ycart.yang@mail.utoronto.ca>
2024-01-22 21:12:57 +0000

    Updated data simulation due to discrepencies between data description and actual data on Open Data Toronto.

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,12 +1,14 @@
 #### Preamble ####
 # Purpose: Simulates 2019 data of Person in Crisis (PIC) calls for service 
-# attended (CFSA) that includes event types of Attempted Suicide, Person In 
-# Crisis, Elopee, Jumper, Overdose, and Threaten Suicide.
+# attended (CFSA) that includes event types of Suicide-related, Person In 
+# Crisis, and Overdose.
 # Author: Tracy yang
 # Date: 23 January 2024
 # Contact: ycart.yang@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: None
+# The website states that there are six event types. However, the data 
+# categorizes three. The simulation will include three categories.
 
 
 #### Workspace setup ####
@@ -15,11 +17,10 @@ library(tidyverse)
 #### Simulate data ####
 set.seed(11)
 
-year <- c(2018, 2019, 2020)
+year <- c(2019, 2020, 2021)
 month <- c("Janaury", "February", "March", "April", "May", "June", "July", 
            "August", "September", "October", "November", "December")
-event <- c("Attempted Suicide", "Person In Crisis", "Elopee", "Jumper", 
-           "Overdose", "Threaten Suicide")
+event <- c("Suicide Related", "Person In Crisis", "Overdose")
 
 simulated_toronto <- tibble(year = rep(year, each = 4800), 
             month = rep(month, each = 400, times = 3), 
@@ -29,5 +30,5 @@ simulated_toronto <- tibble(year = rep(year, each = 4800),
 simulated_toronto$year |> class() == "numeric"
 simulated_toronto$month |> class() == "character"
 simulated_toronto$month |> unique() |> length() == 12
-simulated_toronto$event_type |> unique() |> length() == 6
+simulated_toronto$event_type |> unique() |> length() == 3
 

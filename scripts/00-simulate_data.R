733dc06e861d04243a3bb516557feb613e01fb70
Tracy Yang <ycart.yang@mail.utoronto.ca>
2024-01-22 17:39:55 +0000

    Finished updating preamble, simulating data, and conducted tests.

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,34 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates 2019 data of Person in Crisis (PIC) calls for service 
+# attended (CFSA) that includes event types of Attempted Suicide, Person In 
+# Crisis, Elopee, Jumper, Overdose, and Threaten Suicide.
+# Author: Tracy yang
+# Date: 23 January 2024
+# Contact: ycart.yang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
+# Pre-requisites: None
 # Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(11)
 
+year <- c(2018, 2019, 2020)
+month <- c("Janaury", "February", "March", "April", "May", "June", "July", 
+           "August", "September", "October", "November", "December")
+event <- c("Attempted Suicide", "Person In Crisis", "Elopee", "Jumper", 
+           "Overdose", "Threaten Suicide")
 
+simulated_toronto <- tibble(year = rep(year, each = 4800), 
+            month = rep(month, each = 400, times = 3), 
+            event_type = sample(event, 14400, replace = TRUE))
+
+#### Testing Simulated Data ####
+simulated_toronto$year |> class() == "numeric"
+simulated_toronto$month |> class() == "character"
+simulated_toronto$month |> unique() |> length() == 12
+simulated_toronto$event_type |> unique() |> length() == 6
 
